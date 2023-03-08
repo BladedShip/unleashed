7d35c2fdfca654f4e9dd7fd202055f4b1f501efd
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-03-08 21:45:06 +0530

    banner and header complete

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HeartIcon } from "@heroicons/react/24/solid";
+// import { HeartIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -8,13 +8,13 @@ function Banner({}: Props) {
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
       <div>
         <h1 className="text-7xl">Thoughts Unleashed</h1>
-        <h2 className="mt-5">
-          A journey through my thoughts and ideas
+        <h2 className="mt-5 font-medium">
+          A journey through my <span className="underline decoration-[#dbbadd] decoration-2 font-bold">thoughts</span> and <span className="underline font-bold decoration-[#dbbadd] decoration-2">ideas</span>
         </h2>
       </div>
       <p className="mt-5 md:mt-2 text-[#fcfcfc]/50 max-w-sm">
-        Built using Next.js 13 and Sanity CMS with{" "}
-        <HeartIcon className="w-4 h-4 inline-block" />
+        Built using Next.js 13 and Sanity CMS
+        {/* <HeartIcon className="w-4 h-4 inline-block" /> */}
         <br />
         by Adithyan Jayakumar
       </p>
